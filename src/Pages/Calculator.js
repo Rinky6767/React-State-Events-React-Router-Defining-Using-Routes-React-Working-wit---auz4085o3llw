63421b44f0cc125420cb6423a4f819f787402f38
Rinky <71222541+Rinky6767@users.noreply.github.com>
2022-06-28 19:17:40 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
+export const Calculator = () => {
+  let param = useSearchParams();
+  let data = param[0];
+  let num1 = data.get("num1");
+  let num2 = data.get("num2");
+  let op = unescape(data.get("op"));
+  let res;
+  switch (op) {
+    case "+":
+      res = num1 + num2;
+      break;
 
-export const Calculator = () =>{
+    case "-":
+      res = num1 - num2;
+      break;
 
-    return(
-        
-    )
-}
+    case "*":
+      res = num1 * num2;
+      break;
+
+    case "/":
+      res = num1 / num2;
+      break;
+
+    case "%":
+      res = num1 % num2;
+      break;
+
+    default:
+      res = 0;
+  }
+
+  return (
+    <>
+      <div>
+        Your calculation Result is
+        <span id="calc-result"> {res}</span>
+      </div>
+    </>
+  );
+};
